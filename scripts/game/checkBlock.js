f9b7f3ea599fbca1e00345cbc2e274d340326b04
Oscar Rodriguez <oscarrobertrodriguez@gmail.com>
2019-07-19 14:55:08 -0500

    implemeted checkBlock function correctly

---
+++ scripts/game/checkBlock.js
@@ -0,0 +1,39 @@
+export default function checkBlock(e) {
+  var target = e.target;
+  var letterColKey = target.dataset.square.split("")[0];
+  var allColSquares = document.querySelectorAll(
+    `input[data-square^=${letterColKey}`
+  );
+
+  // returns all current values for column into array
+  var nonFilterValues = [...allColSquares]
+    .map(function checkColumnHandler(item) {
+      return item.value;
+    })
+    .join("");
+
+  var uniqueValues = [...new Set(nonFilterValues)];
+
+  // collect all columns with data-column-seleted set to true
+  // this is done so that checkBlock does not overwrite checkColumn
+  var trueRedColumns = document.querySelectorAll(
+    `input[data-column-selected=true]`
+  );
+
+  // if their are more than the same # in block turn whole block red
+  // a block is defined as a group of 9 little squares
+  if (
+    nonFilterValues.length !== uniqueValues.length &&
+    uniqueValues.length > 1
+  ) {
+    allColSquares.forEach(item => {
+      item.style.background = "red";
+    });
+  } else {
+    allColSquares.forEach(item => (item.style.background = ""));
+
+    [...trueRedColumns].forEach(item => {
+      item.style.background = "red";
+    });
+  }
+}
--- scripts/game/checkColumn.js
+++ scripts/game/checkColumn.js
@@ -1,10 +1,10 @@
 export default function checkColumn(e) {
   var target = e.target;
-  var value = target.value;
   var letterColKey = target.dataset.column;
   var allColSquares = document.querySelectorAll(
     `input[data-column=${letterColKey}`
   );
+
   // returns all current values for column
   var nonFilterValues = [...allColSquares]
     .map(function checkColumnHandler(item) {
@@ -12,17 +12,24 @@ export default function checkColumn(e) {
     })
     .join("");
 
+  // use es6 set to filter all non unique values
   var uniqueValues = [...new Set(nonFilterValues)];
 
-  // ir their are more than the same # in column run code
+  // compare original values with unique values if
+  // length is not same then there was a duplicate number so
+  // all column is hightlighted red
   if (
     nonFilterValues.length !== uniqueValues.length &&
     uniqueValues.length > 1
   ) {
     allColSquares.forEach(item => {
       item.style.background = "red";
+      item.setAttribute("data-column-selected", true);
     });
   } else {
-    allColSquares.forEach(item => (item.style.background = ""));
+    allColSquares.forEach(item => {
+      item.style.background = "";
+      item.setAttribute("data-column-selected", false);
+    });
   }
 }
